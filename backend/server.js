91b0110a87cf7d86339fdd0a3cb52ad9f2ef5a5b
omerakram-OK <omerakramofficial@gmail.com>
2022-07-28 23:56:31 +0500

    chat app structure

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv')
+dotenv.config();
+
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, ()=>{
+    console.log(`server is running on port ${PORT}`)
+})
